ec875bde1aca1dff5de46519554260eeaa73f464
onegin279 <34103486+onegin279@users.noreply.github.com>
2017-12-26 22:28:45 +0300

    Update Calculator.java

--- firstproject/src/main/java/Calculator.java
+++ firstproject/src/main/java/Calculator.java
@@ -1,61 +1,72 @@
 package Calc;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Calculator {
     static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        double num1 = getDouble();
-        double num2 = getDouble2();
+
+
+    public static void main(String[] args) throws IOException {
+
+        int num1 = (int) getInt();
+        int num2 = (int) getInt2();
         char operation = getOperation();
-        double result = calc(num1,num2,operation);
-        System.out.println("Результат операции: "+result);
+        try {
+        int result = (int) calc(num1, num2, operation);
+        System.out.println("Результат операции: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Недопустимая операция");
+        }
 
     }
 
-    public static double getDouble(){
+    public static int getInt() {
         System.out.println("Введите первое число: ");
-        double num = 0;
-        if(scanner.hasNextDouble()){
-            num = scanner.nextDouble();
+        int num = 0;
+        if (scanner.hasNextInt()) {
+            num = scanner.nextInt();
         }
         return num;
     }
-    public static double getDouble2(){
+
+    public static int getInt2() {
         System.out.println("Введите второе число: ");
-        double num2 = 0;
-        if(scanner.hasNextDouble()){
-            num2 = scanner.nextDouble();
+        int num2 = 0;
+        if (scanner.hasNextInt()) {
+            num2 = scanner.nextInt();
         }
         return num2;
     }
 
-    public static char getOperation(){
+    public static char getOperation() {
         System.out.println("Выберите операцию (+, -, * ,/): ");
         char operation = 0;
-        if(scanner.hasNext()){
+        if (scanner.hasNext()) {
             operation = scanner.next().charAt(0);
         }
         return operation;
     }
 
-    public static double calc(double num1, double num2, char operation){
-        double result = 0;
-        switch (operation){
+    public static int calc(int num1, int num2, char operation) {
+        int result = 0;
+        switch (operation) {
             case '+':
-                result = num1+num2;
+                result = num1 + num2;
                 break;
             case '-':
-                result = num1-num2;
+                result = num1 - num2;
                 break;
             case '*':
-                result = num1*num2;
+                result = num1 * num2;
                 break;
             case '/':
-                result = num1/num2;
+                result = num1 / num2;
                 break;
         }
         return result;
     }
-}
+
+
+    }
